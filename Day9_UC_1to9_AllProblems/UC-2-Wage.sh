97df07eb3f7a2bbf48906bcb63dbc2cf9a46e856
ArunGawande <100593245+ArunGawande@users.noreply.github.com>
2022-03-03 15:57:00 +0530

    Add files via upload

---
+++ Day9_UC_1to9_AllProblems/UC-2-Wage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+echo "Welcome to Employee Wage Computation Program"
+
+function attendance()
+{
+	Random=$((RANDOM%2))
+	if [ $Random -eq 1 ]
+	 then
+		echo "Employee is present";
+	 else
+		echo "Employee is absent";
+	fi
+}
+
+
+attendance
+
+
+isFullTime=1;
+empRatePerHr=20;
+randomCheck=$((RANDOM%2))
+
+
+if [ $isFullTime -eq $randomCheck ];
+ then
+       empHrs=8;
+
+ else
+       empHrs=0;
+fi
+
+salary=$(($empHrs * $empRatePerHr));
